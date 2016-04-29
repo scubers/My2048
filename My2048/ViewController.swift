2affa361c21894d4af554345a0ad971c9cd90f4c
JMacMini <jmacmini@jmacminidemac-mini.local>
2016-04-29 09:59:19 +0800

    Initial Commit

---
+++ My2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My2048
+//
+//  Created by JMacMini on 16/4/29.
+//  Copyright © 2016年 Jrwong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
